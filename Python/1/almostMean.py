55a6f0e48f05788629c4a425650187b1c80fd416
NiflheimZ-P <siwaphat.gundej@gmail.com>
2023-10-20 00:00:04 +0700

    ignore

--- Python/1/almostMean.py
+++ Python/1/almostMean.py
@@ -1,26 +1,26 @@
-"""Ejudge"""
-def main():
+"""AlmostMean"""
+def mean():
     """AlmostMean"""
-    nisit = []
-    scorenisit = []
-    scoresum = 0
-    numnisit = int(input())
-    for _ in range(numnisit):
-        tmp = input().split()
-        nisit.append(tmp[0])
-        scorenisit.append(float(tmp[1]))
-        tmp = []
-    for i in scorenisit:
-        scoresum += float(i)
-    mean = scoresum/numnisit
-    scoresort = sorted(scorenisit)
-    indexscore = 0
-    while True:
-        if float(scoresort[indexscore]) >= mean:
-            indexscore -= 1
+    loops = int(input())
+    student_num = []
+    score = []
+    check = 0
+    check_minus = []
+    test = []
+    count = 0
+    for _ in range(loops):
+        student_num.append(str(input()).split())
+    for i in student_num:
+        score.append(float(i.pop(-1)))
+        check += (score[-1])
+    check /= loops
+    for j in score:
+        check_minus.append(abs(j-check))
+    test = check_minus.copy()
+    test.sort()
+    for k in check_minus:
+        count += 1
+        if k == test[0]:
             break
-        indexscore += 1
-    for i in range(len(scorenisit)):
-        if float(scorenisit[i]) == float(scoresort[indexscore]):
-            print("%s\t%s"%(nisit[i], str(scorenisit[i])))
-main()
+    print("%s\t%s"%(*student_num[count-1], str(score[count-1])))
+mean()
